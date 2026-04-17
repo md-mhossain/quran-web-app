b4b66c36915985d9849a987d40ceff46d17f59a4
Monir Hossain <monirhdigital@gmail.com>
2026-04-18 05:48:24 +0600

    data url update

--- backend/src/lib/quran.loader.ts
+++ backend/src/lib/quran.loader.ts
@@ -8,7 +8,7 @@ export const loadQuran = async () => {
   console.log("Loading Quran at server startup...");
 
   // const data_url = CONFIG.DATA_URL || "";
-  const data_url = "https://raw.githubusercontent.com/risan/quran-json/master/dist/quran_en.json"
+  const data_url = CONFIG.DATA_URL || "";
 
   const res = await fetch(data_url);
   cache = (await res.json()) as Surah[];
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,9 @@
 import app from "./app";
 import { CONFIG } from './config';
 
-// import { initQuran } from "./lib/quran.loader";
-
 const PORT = CONFIG.PORT || 8000;
 
 const startServer = async () => {
-  // await initQuran();
-
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
   });
