9999bc48c492004378aaa3a0ac9fb99b45ba5d7c
Monir Hossain <monirhdigital@gmail.com>
2026-04-18 05:17:56 +0600

    update file

--- backend/src/lib/quran.loader.ts
+++ backend/src/lib/quran.loader.ts
@@ -2,19 +2,23 @@ import { Surah } from "../types";
 import { CONFIG } from '../config/index';
 
 
-const data_url = CONFIG.DATA_URL || "";
+// const data_url = CONFIG.DATA_URL || "";
 
 let cache: Surah[] = [];
 
 export const initQuran = async () => {
   console.log("Loading Quran at server startup...");
 
-  const res = await fetch(data_url);
+  const res = await fetch("https://raw.githubusercontent.com/risan/quran-json/master/dist/quran_en.json");
   cache = (await res.json()) as Surah[];
 
   console.log("Quran loaded:", cache.length);
 };
 
-export const loadQuran = () => {
+export const loadQuran = (): Surah[] => {
+  if (!cache) {
+    throw new Error("Quran not initialized. Call initQuran first.");
+  }
+
   return cache;
 };
